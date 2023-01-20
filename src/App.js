ddad410a0505e1e060bc3eca4c631b95097cf023
Zosia <zosiaazukowskaa@gmail.com>
2023-01-20 21:03:35 +0100

    errors fixed, files merged

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 
+
 function App() {
+  const [cartIsShown, setCartisShown] = useState(false);
+
+  function showCartHandler(){
+    setCartisShown(true);
+  }
+  
+  function hideCartHandler(){
+    setCartisShown(false);
+  }
+  
   return (
     <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler}/>}
