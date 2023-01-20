02e51c83fca7e606ac6ad2fd2ef6a8a3a758b32d
Zosia <zosiaazukowskaa@gmail.com>
2023-01-20 21:26:26 +0100

    alla-forked

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart/Cart';
 
+
 function App() {
+  const [cartIsShown, setCartisShown] = useState(false);
+
+  function showCartHandler(){
+    setCartisShown(true);
+  }
+  
+  function hideCartHandler(){
+    setCartisShown(false);
+  }
+  
   return (
     <CartProvider>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header  onShowCart = {showCartHandler} />
       <main>
         <Meals />
       </main>
